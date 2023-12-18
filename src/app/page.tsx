088d5c52176126349a41755270c0393f2fe46799
Shivam Singh <shivamks2712@gmail.com>
2023-12-19 02:27:28 +0530

    DB integrated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Header from "./compnents/Header";
 import Board from "./compnents/Board";
-import MyForm from "./compnents/Form";
 export default function Home() {
   return (
     <>
       <Header />
+      <p>hii</p>
       <Board />
     </>
   );
